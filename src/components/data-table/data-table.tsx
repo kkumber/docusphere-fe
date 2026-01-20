d90e9831a2d29e7c664685b06f08ee37fecc95b9
Angel Bert Reyes <angelbertreyes28@gmail.com>
2026-01-20 11:31:03 +0800

    fix

--- src/components/data-table/data-table.tsx
+++ src/components/data-table/data-table.tsx
@@ -94,7 +94,7 @@ export function DataTable<TData, TValue>({
             ))}
           </TableHeader>
           <TableBody>
-            {table.getRowModel().rows?.length ? (
+            {table.getRowModel().rows && table.getRowModel().rows.length > 0 ? (
               table.getRowModel().rows.map((row) => (
                 <TableRow
                   key={row.id}
