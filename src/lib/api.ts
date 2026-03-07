3bfa4b01e1403cf4d4b61142e173c82795a4d442
Angel Bert Reyes <angelbertreyes28@gmail.com>
2026-03-08 01:10:45 +0800

    save

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,5 @@
 import axios from "axios"
+import getCookie from "@/utils/get-cookie";
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
@@ -6,6 +7,18 @@ const api = axios.create({
     withXSRFToken: true,
     xsrfCookieName: "XSRF-TOKEN",
     xsrfHeaderName: "X-XSRF-TOKEN"
+});
+
+api.interceptors.request.use((config) => {
+    const token = document.cookie
+        .split('; ')
+        .find(row => row.startsWith('XSRF-TOKEN='))
+        ?.split('=')[1];
+
+    if (token) {
+        config.headers["X-XSRF-TOKEN"] = decodeURIComponent(token);
+    }
+    return config;
 });
 
 
