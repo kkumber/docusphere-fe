7db2f4cea95ce8fd3d0ab89e37912c14ed7b9fc0
Angel Bert Reyes <angelbertreyes28@gmail.com>
2026-03-08 00:44:12 +0800

    save

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,6 +4,7 @@ import getCookie from "@/utils/get-cookie";
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true,
+    withXSRFToken: true,
     xsrfCookieName: "XSRF-TOKEN",
     xsrfHeaderName: "X-XSRF-TOKEN"
 });
