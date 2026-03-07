735c443d18a343aa4a5566119418d7469643c974
Angel Bert Reyes <angelbertreyes28@gmail.com>
2026-03-08 00:55:33 +0800

    save

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,4 @@
 import axios from "axios"
-import getCookie from "@/utils/get-cookie";
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
@@ -7,14 +6,6 @@ const api = axios.create({
     withXSRFToken: true,
     xsrfCookieName: "XSRF-TOKEN",
     xsrfHeaderName: "X-XSRF-TOKEN"
-});
-
-api.interceptors.request.use((config) => {
-    const token = getCookie("XSRF-TOKEN");
-    if (token) {
-        config.headers["X-XSRF-TOKEN"] = decodeURIComponent(token);
-    }
-    return config;
 });
 
 
